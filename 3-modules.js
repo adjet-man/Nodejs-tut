9eee766714ea05b0babb8d3cdb47b29aad861d4b
Richard Adjetey <richadjetey@gmail.com>
2022-08-16 18:05:36 +0000

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,14 @@
+// Modules
+
+const names = require('./4-names');
+const sayHi = require('./5-utilis');
+//console.log(names)
+
+const flavor = require ('./6-alternative-flavor')
+//console.log(flavor)
+
+require('./7-mind')
+
+sayHi('Jay')
+sayHi(names.john)
+sayHi(names.peter)
